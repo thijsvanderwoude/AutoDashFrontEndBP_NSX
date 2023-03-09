acf3e11296779c475564e435599dbc5aa093bcf9
Thijs van der Woude <thijsvanderwoude99@gmail.com>
2023-03-09 14:54:19 +0100

    Optimize noComm condition TPS bar

--- src/js/tps.js
+++ src/js/tps.js
@@ -6,14 +6,14 @@ export default {
   },
   update: (tpsPercentage, noComm) => {
     if (noComm) {
-      tpsBar.textContent = '<div style="width: 0%" aria-valuenow="0" class="progress-bar bg-white" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div><div style="width: 100%" aria-valuenow="100" class="progress-bar bg-black" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>';
+      tpsBar.textContent = '<div style="width: 100%" aria-valuenow="100" class="progress-bar bg-black" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>';
       return;
     }
     
     /*
 
     Generate a Bootstrap progress bar according to the number given.
-    
+
     Example:
 
     <div style="width: 65%" aria-valuenow="65" class="progress-bar bg-white" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
@@ -25,7 +25,5 @@ export default {
       + tpsPercentage + '" class="progress-bar bg-white" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div><div style="width: '
       + (100 - tpsPercentage) + '%" aria-valuenow="'
       + (100 - tpsPercentage) + '" class="progress-bar bg-black" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>';
-
-
   },
 };
