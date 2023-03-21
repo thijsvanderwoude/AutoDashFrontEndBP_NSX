bf109a66c036b5af1f3441bd4aff6d16869d9a61
Thijs van der Woude <thijsvanderwoude99@gmail.com>
2023-03-21 23:45:28 +0100

    Change tire diameter

--- src/js/speedo.js
+++ src/js/speedo.js
@@ -1,5 +1,5 @@
 let speedo = null;
-const tireDiameter = 24.9;    // https://tiresize.com/calculator/ to calculate diameter of the rear tire
+const tireDiameter = 26.3;    // https://tiresize.com/calculator/ to calculate diameter of the rear tire
 const gearRatio = 9.71;       // Tesla large drive unit gear ratio according to tesla.com?
 
 export default {
@@ -19,6 +19,7 @@ export default {
      Vehicle speed (mph) = wheel RPM × tire diameter × π × 60 / 63360
      Wheel RPM = engine RPM / gear ratio / diff ratio
     */
+    
     var wheelRPM = rpm / gearRatio;
     var vehicleSpeed = wheelRPM * tireDiameter * Math.PI * 60 / 63360;
     speedo.innerHTML = Math.round(vehicleSpeed);
